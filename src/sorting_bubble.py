e100bb0ad19ab30690a8c9dd5dd91655eb27c923
Pavel Nartov <pnartov@gmail.com>
2022-08-16 11:06:08 +0300

    Improve sorting bubble

--- src/sorting_bubble.py
+++ src/sorting_bubble.py
@@ -1,11 +1,11 @@
-origin = [5, 7, 2, 9, 33, 12, 1, 4, 22, 1, 4]
+origin = [5, 7, 2, 9, 33, 12, 1, 4, 22, 1, 4, 1]
 
 
 def sort_bubble(list: list):
     a = list.copy()
     length = len(a)
     for j in range(length):
-        for i in range(length - 1):
+        for i in range(length - j - 1):
             if a[i] > a[i + 1]:
                 a[i], a[i + 1] = a[i + 1], a[i]
     return a
