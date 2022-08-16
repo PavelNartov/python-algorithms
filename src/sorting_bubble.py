07e291a6dda085930019e7ce59753a2f14156d3e
Pavel Nartov <pnartov@gmail.com>
2022-08-16 10:55:58 +0300

    Add sorting_bubble algorithm implementation

---
+++ src/sorting_bubble.py
@@ -0,0 +1,17 @@
+origin = [5, 7, 2, 9, 33, 12, 1, 4, 22, 1, 4]
+
+
+def sort_bubble(list: list):
+    a = list.copy()
+    length = len(a)
+    for j in range(length):
+        for i in range(length - 1):
+            if a[i] > a[i + 1]:
+                a[i], a[i + 1] = a[i + 1], a[i]
+    return a
+
+
+b = sort_bubble(origin)
+
+print(origin)
+print(b)
